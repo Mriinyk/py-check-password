b18738b637be3b5b293a0e723c684ce2a8420f58
Arseniy Petrenko <arsfix2004@gmail.com>
2026-03-26 18:45:35 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        pytest.param("Pass@word", False, id="no number"),
+        pytest.param("password1@", False, id="no capital letter"),
+        pytest.param("PASS1@WORD", False, id="no lowercase letter"),
+        pytest.param("Pass word1@", False, id="there is a space"),
+        pytest.param("Pass@1", False, id="less than 8 characters"),
+        pytest.param("Pass@word1word1word", False,
+                     id="more than 16 characters"),
+        pytest.param("Pass@word1", True, id="all good")
+    ]
+)
+def test_check_password(password: str, expected: bool) -> None:
+    assert check_password(password) == expected
